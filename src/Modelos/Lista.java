1500710c0280e519441d5ea89ab09567b39974ba
edhelmont <arellanonegrin2@hotmail.com>
2015-03-19 21:38:00 -0430

    empaquetados
    se cambio el admin de protegido a publico por conflicto de que estaban
    en el mismo paquete cuando ellos iban en diferentes

---
+++ src/Modelos/Lista.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Loucry
+ */
+public class Lista {
+    private String Nombre;
+    private int NRutas;
+    ArrayList <Rutas> RutasList;
+    
+    public Lista(String name){
+        Nombre = name;
+        NRutas = 0;
+        RutasList = new ArrayList <>();
+    }
+}
